353075992090acc8519d395c654f39fb106c1670
Amin1014 <aminabdi570@gmail.com>
2021-10-18 10:43:11 +0300

    solving password reset error

--- stockInvent/settings.py
+++ stockInvent/settings.py
@@ -80,6 +80,16 @@ TEMPLATES = [
     },
 ]
 
+SWAGGER_SETTINGS={
+    'SECURITY_DEFINITIONS':{
+        'Bearer':{
+            'type':'apiKey',
+            'name':'Authorization',
+            'in':'header'
+        }
+    }
+}
+
 WSGI_APPLICATION = 'stockInvent.wsgi.application'
 # REST_FRAMEWORK = {
 #     'NON_FIELD_ERRORS_KEY': 'error',
@@ -106,9 +116,7 @@ WSGI_APPLICATION = 'stockInvent.wsgi.application'
 #     ),
 # }
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    ),
+    
     'DEFAULT_AUTHENTICATION_CLASSES': (
         
         'rest_framework_simplejwt.authentication.JWTAuthentication',
@@ -226,6 +234,12 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
